33e55b379dfe49f4bad08b6d324c3f372ab34c9c
gameme10 <66317738+gameme10@users.noreply.github.com>
2021-03-12 16:34:50 +0100

    audio v2

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ import os, datetime, time, pygame
 from threading import Thread
 alarms = {
     #"food1": "21:00",
-    "food": "9:48",
+    "food": "15:28",
     "power": "True"
 }
 clear = lambda: os.system('cls')
+def playdog(dogWAV):
+    pygame.mixer.init()
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    sound = pygame.mixer.Sound(os.path.join(dir_path, dogWAV))
+    sound.play()
 def tfdt():
     while True:
         lcltime = datetime.datetime.now().strftime('%H:%M')
         if lcltime == alarms["food"]:
-            pygame.mixer.init()
-            dir_path = os.path.dirname(os.path.abspath(__file__))
-            sound = pygame.mixer.Sound(os.path.join(dir_path, 'dog1.wav'))
-            sound.play()
+            playdog('alarms/dog1.wav')
+            time.sleep(10)
             break
         else:
             time.sleep(10)
@@ -29,12 +32,19 @@ def actions(name):
     print("\n \n \n")
     if(str(name).lower() == "sleep"):
         printDog("dogSleep")
+        playdog("alarms/dogSleeping.wav")
+    elif(str(name).lower()=="help"):
+        fileCmds = open("cmds.txt", "r")
+        for line in fileCmds.readlines():
+            print(line.replace('\n', ''))
+        fileCmds.close()
     elif(str(name).lower()=="eat"):
         printDog("dogEat")
         print("Pulsa ENTER para darle de comer")
         input()
         clear()
         printDog("dogEat1")
+        playdog("alarms/dogEating.wav")
     elif(str(name).lower()=="config"):
         print("Configuracion \nActivar/Desactivar alarmas = alarms.<True/False>")
         opcinoncofig = input()
@@ -43,9 +53,7 @@ def actions(name):
     
     elif(str(name).lower() == "exit"):
         clear()
-        tfdtThread.do_run = False
-        clear()
-        sys.exit("Adios :D")
+        print("Pulsa Ctrl + C y luego ENTER para cerrar")
         
     else:
         print("ERROR: El comando no es valido")
@@ -54,13 +62,10 @@ def actions(name):
 
 def onStart():
     clear()
+    playdog("alarms/dog1.wav")
     print("\n \n \n")
     printDog("dog1")
-    print("\n\n+-------------+\nLista de comandos: ")
-    fileCmds = open("cmds.txt", "r")
-    for line in fileCmds.readlines():
-        print(line.replace('\n', ''))
-    fileCmds.close()
+    print("\n\n+-------------+\nEscribe 'help' para ver la lista de comandos: ")
     print("Que comando desea ejecutar: ")
     command = input()
     actions(command)
